6a3c40baac27505604ce53e98a1848b30ae7971c
ndoliveiraribeiro-gif <n.doliveira.ribeiro@gmail.com>
2026-06-15 01:13:14 +0100

    fix checkout page

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,3 +1,7 @@
 export default function Checkout() {
-  return <div>Checkout em construção</div>;
+  return (
+    <div style={{minHeight:'100vh',display:'flex',alignItems:'center',justifyContent:'center',fontFamily:"'Jost',sans-serif"}}>
+      <p>Checkout em construção</p>
+    </div>
+  );
 }
